14da80f41edb8824b14acf9383a9db90326ab67d
RicardViana <ricardviana1@gmail.com>
2024-03-20 17:45:23 +0000

    arquivo gasolina.png alterado

--- gasolina.py
+++ gasolina.py
@@ -5,7 +5,7 @@ preco_gasolina = pd.read_csv('gasolina.csv', sep=',')
 with sns.axes_style('whitegrid'):
 
   grafico = sns.lineplot(data=preco_gasolina, x="dia", y="venda")
-  grafico.set(title='Preço médio de venda da gasolina na cidade de São Paulo', xlabel='Dia', ylabel='Venda');
+  grafico.set(title='Preço médio da gasolina em São Paulo', xlabel='Dia', ylabel='Preço');
 
   fig = grafico.get_figure()
   fig.savefig(fname="gasolina.png", bbox_inches="tight")
